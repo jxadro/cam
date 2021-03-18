2fb5d802d7f4c412aaed2b34a6b6c1f4131468e0
Javier Ordax <javier.ordax@es.ibm.com>
2021-03-18 15:56:12 +0100

    1

---
+++ vpc_gen2_instance/main.tf
@@ -0,0 +1,66 @@
+locals {
+  BASENAME = "jordax"
+  ZONE     = "us-south-1"
+}
+
+provider "ibm" {
+  region = var.region
+  generation = 2
+  resource_group = var.resource_group
+}
+
+data "ibm_is_vpc" "vpc" {
+  name = var.vpc
+}
+
+data "ibm_is_security_group" "sg1" {
+  name = var.security_group
+}
+
+
+data "ibm_is_subnet" "subnet1" {
+  name = var.subnet
+}
+
+data "ibm_is_image" "ubuntu" {
+  name = "ibm-centos-7-6-minimal-amd64-2"
+}
+
+data "ibm_is_ssh_key" "ssh_key_id" {
+  name = var.ssh_key_name
+}
+
+data "ibm_resource_group" "group" {
+  name = var.resource_group
+}
+
+
+resource "ibm_is_instance" "vsi1" {
+  name    = "${local.BASENAME}-${var.instance_name}"
+  vpc     = data.ibm_is_vpc.vpc.id
+  zone    = local.ZONE
+  keys    = [data.ibm_is_ssh_key.ssh_key_id.id]
+  image   = data.ibm_is_image.ubuntu.id
+  profile = var.size
+  resource_group = data.ibm_resource_group.group.id
+
+  primary_network_interface {
+    subnet          = data.ibm_is_subnet.subnet1.id
+    security_groups = [data.ibm_is_security_group.sg1.id]
+  }
+}
+
+resource "ibm_is_floating_ip" "fip1" {
+  name   = "${local.BASENAME}-${var.instance_name}-fip1"
+  target = ibm_is_instance.vsi1.primary_network_interface[0].id
+}
+
+
+output "ip" {
+  value = ibm_is_floating_ip.fip1.address
+}
+
+output "ssh" {
+  value = ibm_is_ssh_key.ssh_key_id.fingerprint
+}
+
---
+++ vpc_gen2_instance/variables.tf
@@ -0,0 +1,40 @@
+variable "ssh_key_name" {
+  type        = string
+  description = "SSH Key Name in IBM Cloud"
+}
+
+variable "instance_name" {
+  type        = string
+  description = "Server instance name"
+}
+
+variable "region" {
+  type        = string
+  description = "VPC Region"
+}
+
+variable "resource_group" {
+  type        = string
+  description = "Resource Group"
+}
+
+variable "vpc" {
+  type        = string
+  description = "VPC Name"
+}
+
+variable "security_group" {
+  type        = string
+  description = Security Group Name"
+}
+
+variable "subnet" {
+  type        = string
+  description = "Subnet name"
+}
+
+variable "size" {
+  type        = string
+  description = "IBM Cloud Profile"
+}
+
