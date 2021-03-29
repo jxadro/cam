44b2b80bf93e61020be24fbc69b22b943fc01a63
Javier Ordax <javier.ordax@es.ibm.com>
2021-03-29 14:27:18 +0200

    1

---
+++ approval/main.tf
@@ -0,0 +1,57 @@
+variable "host" {
+    description = "URL to retrieve approval status"
+}
+
+variable "catalogid" {
+    description = "Catalg Id"
+}
+
+variable "itemid" {
+    description = "Item Id"
+}
+
+data "http" "create" {
+  url = "http://post2get-post2get.apps.hubcluster.mcmpoc.com/?catalogid=${var.catalogid}&itemid=${var.itemid}&host=${var.host}"
+
+  # Optional request headers
+  request_headers = {
+    Accept = "application/json"
+  }
+}
+
+
+
+resource "local_file" "approval_status" {
+    content   = ""
+    filename  = "${path.module}/approval_status"
+}
+
+#########################################################
+# Poll Infrastructure Management for approval status
+#########################################################
+resource "null_resource" "poll_endpoint" {
+ provisioner "local-exec" {
+    command = "/bin/bash poll_endpoint.sh $URL $USERNAME $PASSWORD $CURL_OPTIONS $WAIT_TIME $FILE"
+    environment = {
+      URL          = jsondecode(data.http.create.body).href
+      USERNAME     = "admin"
+      PASSWORD     = "n0r1t5@C"
+      CURL_OPTIONS = "--insecure"
+      WAIT_TIME    = 5
+      FILE         = "${path.module}/approval_status"
+    }
+  }
+  depends_on = [
+    local_file.approval_status
+  ]
+}
+
+#########################################################
+# Output
+#########################################################
+output "approval_status" {
+  value = "${fileexists("${local_file.approval_status.filename}") ? file("${local_file.approval_status.filename}") : ""}"
+  depends_on = [
+    null_resource.poll_endpoint
+  ]
+}
